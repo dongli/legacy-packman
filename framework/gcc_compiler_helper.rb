0222577cac1aa3d5b13014778c88199f3171d9b4
Li Dong <dongli@lasg.iap.ac.cn>
2014-08-28 16:16:15 +0800

    Replace '-O3' to '-O2'

--- framework/gcc_compiler_helper.rb
+++ framework/gcc_compiler_helper.rb
@@ -1,7 +1,7 @@
 module PACKMAN
   class GccCompilerHelper < CompilerHelper
     vendor 'gcc'
-    default_flags '-O3'
+    default_flags '-O2'
     compiler_commands({ 'c' => 'gcc', 'c++' => 'g++', 'fortran' => 'gfortran' })
     version_pattern /\d+\.\d+\.\d+/
   end
--- framework/intel_compiler_helper.rb
+++ framework/intel_compiler_helper.rb
@@ -1,7 +1,7 @@
 module PACKMAN
   class IntelCompilerHelper < CompilerHelper
     vendor 'intel'
-    default_flags '-O3 -ip -fpic'
+    default_flags '-O2 -ip -fpic'
     compiler_commands({ 'c' => 'icc', 'c++' => 'icpc', 'fortran' => 'ifort' })
     version_pattern /\d+\.\d+(\.\d+)?/
   end
