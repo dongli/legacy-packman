5da558e246cfdafb1d9ce34b1c2196e301e473e1
Li Dong <dongli@lasg.iap.ac.cn>
2014-09-04 09:54:41 +0800

    Turn on OpenDAP support in Netcdf
    When telling Netcdf the location of Curl, it will turn on OpenDAP
    support.

--- packages/netcdf_c.rb
+++ packages/netcdf_c.rb
@@ -3,16 +3,18 @@ class Netcdf_c < PACKMAN::Package
   sha1 '6e1bacab02e5220954fe0328d710ebb71c071d19'
   version '4.3.2'
 
+  depends_on 'curl'
   depends_on 'zlib'
   depends_on 'szip'
   depends_on 'hdf5'
 
   def install
+    curl_prefix = PACKMAN::Package.prefix(Curl)
     zlib_prefix = PACKMAN::Package.prefix(Zlib)
     szip_prefix = PACKMAN::Package.prefix(Szip)
     hdf5_prefix = PACKMAN::Package.prefix(Hdf5)
-    PACKMAN.append_env "CFLAGS='-I#{zlib_prefix}/include -I#{szip_prefix}/include -I#{hdf5_prefix}/include'"
-    PACKMAN.append_env "LDFLAGS='-L#{zlib_prefix}/lib -L#{szip_prefix}/lib -L#{hdf5_prefix}/lib'"
+    PACKMAN.append_env "CFLAGS='-I#{curl_prefix}/include -I#{zlib_prefix}/include -I#{szip_prefix}/include -I#{hdf5_prefix}/include'"
+    PACKMAN.append_env "LDFLAGS='-L#{curl_prefix}/lib -L#{zlib_prefix}/lib -L#{szip_prefix}/lib -L#{hdf5_prefix}/lib'"
     PACKMAN.append_env "FFLAGS=-ffree-line-length-none"
     args = %W[
       --prefix=#{PACKMAN::Package.prefix(self)}
