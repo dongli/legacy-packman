62f39bbbb30aca742b7a7108941bc7a228a73789
Li Dong <dongli@lasg.iap.ac.cn>
2014-10-23 11:12:22 +0800

    Fix remove subcommand
    Previously, there will be remaining empty directories. Now they are gone.

--- framework/command/remove.rb
+++ framework/command/remove.rb
@@ -3,10 +3,11 @@ module PACKMAN
     def self.remove
       CommandLine.packages.each do |package_name|
         package = Package.instance package_name
-        if not File.directory? "#{ConfigManager.install_root}/#{package_name.to_s.downcase}"
+        package_root = "#{ConfigManager.install_root}/#{package_name.to_s.downcase}"
+        if not File.directory? package_root
           CLI.report_error "Package #{CLI.red package_name} is not installed!"
         end
-        versions = Dir.glob("#{ConfigManager.install_root}/#{package_name.to_s.downcase}/*").sort
+        versions = Dir.glob("#{package_root}/*").sort
         if versions.size > 1 and not CommandLine.has_option? '-all'
           CLI.report_warning "Multiple versions of package #{CLI.red package_name} have been installed."
           tmp = versions.map { |v| File.basename(v) }
@@ -25,6 +26,7 @@ module PACKMAN
           if removed_versions.include? j or removed_versions.include? versions.size
             if not package.has_label? 'compiler_insensitive'
               sets = Dir.glob("#{versions[j]}/*").sort
+              removed_sets = []
               if sets.size > 1 and not CommandLine.has_option? '-all'
                 CLI.report_warning "Package #{CLI.red package_name} (#{File.basename versions[j]}) "+
                   "has been compiled by multiple compiler sets."
@@ -33,27 +35,32 @@ module PACKMAN
                 CLI.ask 'Which set do you want to remove?', tmp
                 removed_sets = CLI.get_answer tmp
               elsif sets.size == 1
-                removed_sets = [0]
+                removed_sets << 0
               elsif CommandLine.has_option? '-all'
-                removed_sets = []
                 for i in 0..sets.size-1
                   removed_sets << i
                 end
-              else
-                CLI.report_error "Unexpected situation!"
               end
               for i in 0..ConfigManager.compiler_sets.size-1
                 if removed_sets.include? i or removed_sets.include? ConfigManager.compiler_sets.size
                   CLI.report_notice "Remove #{CLI.red sets[i]}."
+                  PACKMAN.delete_from_file "#{ConfigManager.install_root}/packman.bashrc",
+                    "source #{sets[i]}/bashrc", :no_error
                   PACKMAN.rm sets[i]
                 end
               end
             else
               CLI.report_notice "Remove #{CLI.red versions[j]}."
+              PACKMAN.delete_from_file "#{ConfigManager.install_root}/packman.bashrc",
+                "source #{versions[i]}/bashrc", :no_error
               PACKMAN.rm versions[j]
             end
+            # Remove empty directory if there is.
+            PACKMAN.rm versions[j] if PACKMAN.is_directory_empty? versions[j]
           end
         end
+        # Remove empty directory if there is.
+        PACKMAN.rm package_root if PACKMAN.is_directory_empty? package_root
       end
     end
   end
