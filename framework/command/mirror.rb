82323149860d707c5ee4edd5492d67c538934d83
Li Dong <dongli@lasg.iap.ac.cn>
2014-10-27 15:15:40 +0800

    Fix an occasion in mirror subcommand
    In some occasion, the pid file of proftpd is gone, but proftpd deamon
    is still on. So improve the status_mirror_service and stop_mirror_service
    methods to handle this occasion.

--- framework/command/mirror.rb
+++ framework/command/mirror.rb
@@ -1,3 +1,5 @@
+require "net/ftp"
+
 module PACKMAN
   class Commands
     def self.mirror
@@ -31,6 +33,10 @@ module PACKMAN
     end
 
     def self.start_mirror_service
+      if status_mirror_service :silent
+        CLI.report_notice 'FTP mirror service is already on.'
+        return
+      end
       Package.compiler_set = ConfigManager.compiler_sets[0]
       CLI.report_notice "Start FTP mirror service."
       cmd = "#{PACKMAN.prefix(Proftpd)}/sbin/proftpd"
@@ -42,14 +48,29 @@ module PACKMAN
     end
 
     def self.stop_mirror_service
+      if not status_mirror_service :silent
+        CLI.report_notice 'FTP mirror service is already off.'
+        return
+      end
       Package.compiler_set = ConfigManager.compiler_sets[0]
-      pid = "#{PACKMAN.prefix(Proftpd)}/var/proftpd.pid"
-      if not File.exist? pid
-        CLI.report_warning "FTP mirror service is not running!"
-        exit
+      pid_file = "#{PACKMAN.prefix(Proftpd)}/var/proftpd.pid"
+      if not File.exist? pid_file
+        pid = `pgrep proftpd`.split("\n")
+        if pid.size == 1
+          pid = pid.first
+          CLI.report_warning "#{CLI.red pid_file} does not exist, but there is another "+
+            "#{CLI.red 'proftpd'} process (#{CLI.red pid})."
+          CLI.ask 'Do you want to stop it?', ['yes', 'no']
+          ans = CLI.get_answer ['yes', 'no']
+          return if ans != [0]
+        else
+          CLI.report_error "There are multiple #{CLI.red 'proftpd'} processes!"
+        end
+      else
+        pid = `cat #{pid_file}`
       end
       CLI.report_notice "Stop FTP mirror service."
-      cmd = "kill -TERM `cat #{pid}`"
+      cmd = "kill -TERM #{pid}"
       if ENV['USER'] != 'root'
         system "sudo #{cmd}"
       else
@@ -57,13 +78,28 @@ module PACKMAN
       end
     end
 
-    def self.status_mirror_service
-      Package.compiler_set = ConfigManager.compiler_sets[0]
-      pid = "#{PACKMAN.prefix(Proftpd)}/var/proftpd.pid"
-      if File.exist? pid
-        CLI.report_notice "FTP mirror service is #{CLI.green 'on'}."
-      else
-        CLI.report_notice "FTP mirror service is #{CLI.red 'off'}."
+    def self.status_mirror_service options = []
+      options = [options] if not options.class == Array
+      begin
+        proftpd = Net::FTP.new 'localhost'
+      rescue Errno::ECONNREFUSED
+        CLI.report_notice "FTP mirror service is #{CLI.red 'off'}." if not options.include? :silent
+        return false
+      end
+      begin
+        proftpd.login
+      rescue Net::FTPPermError
+        CLI.report_error "There is another FTP server on this computer and it does not belong to PACKMAN!"
+      end
+      begin
+        if proftpd.status =~ /PACKMAN FTP Mirror Service/
+          CLI.report_notice "FTP mirror service is #{CLI.green 'on'}." if not options.include? :silent
+          return true
+        else
+          CLI.report_error "There is another FTP server on this computer and it does not belong to PACKMAN!"
+        end
+      rescue
+        CLI.report_error "There is another FTP server on this computer and it does not belong to PACKMAN!"
       end
     end
 
