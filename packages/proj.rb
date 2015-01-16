4b57d52360b758e73ff74975fbc8d561ab194cbf
Li Dong <dongli@lasg.iap.ac.cn>
2015-01-16 16:34:17 +0800

    Fix Proj
    Careless Proj developers just wrote wrong header file name in code, and
    this typo exists for more than three years!

--- packages/proj.rb
+++ packages/proj.rb
@@ -5,9 +5,9 @@ class Proj < PACKMAN::Package
 
   def install
     # To avoid the 'sign' function conflict with Hdf_eos2.
-    PACKMAN.replace 'src/PJ_healpix.c', {
-      /sign\s*\(/ => 'sign_in_proj('
-    }
+    PACKMAN.replace 'src/PJ_healpix.c', /sign\s*\(/ => 'sign_in_proj('
+    # Fix the wrong file name, shame on Proj developers.
+    PACKMAN.replace 'src/jniproj.c', 'org_proj4_PJ.h' => 'org_proj4_Projections.h'
     args = %W[
       --prefix=#{PACKMAN.prefix(self)}
       --enable-static=yes
