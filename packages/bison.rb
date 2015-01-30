96d10a24483d91501a461cf413b7c75bd8852477
Li Dong <dongli@lasg.iap.ac.cn>
2015-01-30 15:46:02 +0800

    Install Bison

--- packages/bison.rb
+++ packages/bison.rb
@@ -1,19 +1,19 @@
 class Bison < PACKMAN::Package
-  label 'should_provided_by_system'
+  url 'http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.xz'
+  sha1 '8270497aad88c7dd4f2c317298c50513fb0c3c8e'
+  version '3.0.4'
 
-  def installed?
-    if PACKMAN::OS.mac_gang?
-      return File.exist? '/usr/bin/bison'
-    else
-      return PACKMAN::OS.installed? 'bison'
-    end
-  end
+  depends_on 'libiconv'
+  depends_on 'gettext'
 
-  def install_method
-    if PACKMAN::OS.mac_gang?
-      return 'You should install Xcode and command line tools.'
-    else
-      return PACKMAN::OS.how_to_install 'bison'
-    end
+  def install
+    args = %W[
+      --prefix=#{PACKMAN.prefix self}
+      --with-libiconv-prefix=#{PACKMAN.prefix Libiconv}
+      --with-libintl-prefix=#{PACKMAN.prefix Gettext}
+    ]
+    PACKMAN.run './configure', *args
+    PACKMAN.run 'make -j2'
+    PACKMAN.run 'make install'
   end
 end
