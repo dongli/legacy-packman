d19ae6d110ac53e160ecae8d94f3a3b74b70957a
Li Dong <dongli@lasg.iap.ac.cn>
2015-02-25 22:26:31 +0800

    Fix Wrf_model
    Forget to change append_env in Wrf_model.

--- packages/wrf_model.rb
+++ packages/wrf_model.rb
@@ -59,10 +59,10 @@ class Wrf_model < PACKMAN::Package
       end
     end
     # Set compilation environment.
-    PACKMAN.append_env "CURL_PATH='#{Curl.prefix}'"
-    PACKMAN.append_env "ZLIB_PATH='#{Zlib.prefix}'"
-    PACKMAN.append_env "HDF5_PATH='#{Hdf5.prefix}'"
-    PACKMAN.append_env "NETCDF='#{Netcdf.prefix}'"
+    PACKMAN.append_env 'CURL_PATH', Curl.prefix
+    PACKMAN.append_env 'ZLIB_PATH', Zlib.prefix
+    PACKMAN.append_env 'HDF5_PATH', Hdf5.prefix
+    PACKMAN.append_env 'NETCDF', Netcdf.prefix
     # Check input parameters.
     if not ['serial', 'smpar', 'dmpar', 'dm+sm'].include? build_type
       PACKMAN.report_error "Invalid build type #{PACKMAN.red build_type}!"
