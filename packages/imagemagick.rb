373f0adaf3c7623ad2244765047fc9a84eab89ee
Li Dong <dongli@lasg.iap.ac.cn>
2015-04-08 12:37:41 +0800

    Update Imagemagick to 6.9.1-1

--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -1,7 +1,7 @@
 class Imagemagick < PACKMAN::Package
-  url 'http://www.imagemagick.org/download/releases/ImageMagick-6.8.9-8.tar.xz'
-  sha1 '5304855f3504994ff8bbf380e9a89a9e1dfe8834'
-  version '6.8.9-8'
+  url 'http://www.imagemagick.org/download/releases/ImageMagick-6.9.1-1.tar.xz'
+  sha1 '77e2dc47f87ec1c5b2cb519f298acf1f8202d7d8'
+  version '6.9.1-1'
 
   label 'compiler_insensitive'
 
