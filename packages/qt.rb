f19d81e8a6c2355d305128c9aa1c6b3462532b54
Li Dong <dongli@lasg.iap.ac.cn>
2015-06-15 17:10:57 +0800

    Add Qt 4.8.7
    The build does not succeed yet.

---
+++ packages/qt.rb
@@ -0,0 +1,39 @@
+class Qt < PACKMAN::Package
+  url 'https://download.qt.io/official_releases/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.tar.gz'
+  sha1 '76aef40335c0701e5be7bb3a9101df5d22fe3666'
+  version '4.8.7'
+
+  def install
+    args = %W[
+      -prefix #{prefix}
+      -qt-libtiff
+      -qt-libpng
+      -qt-libjpeg
+      -confirm-license
+      -opensource
+      -nomake demos
+      -nomake examples
+      -fast
+      -release
+    ]
+    if PACKMAN.mac?
+      args << '-cocoa' 
+      if PACKMAN.compiler('c').vendor == 'llvm'
+        args << '-platform'
+        if PACKMAN.os_version >= '10.9'
+          args << 'unsupported/macx-clang-libc++'
+        else
+          args << 'unsupported/macx-clang'
+        end
+      end
+    end
+    if PACKMAN.x86_64?
+      args << '-arch x86_64'
+    else
+      args << '-arch x86'
+    end
+    PACKMAN.run './configure', *args
+    PACKMAN.run 'make -j2'
+    PACKMAN.run 'make install'
+  end
+end
