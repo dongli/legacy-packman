5b364beaf4fc77b900debdaa6e916613b9558d0a
Li Dong <dongli@lasg.iap.ac.cn>
2015-07-14 22:00:30 +0800

    Add Ruby_sequel 4.24.0

---
+++ packages/ruby_sequel.rb
@@ -0,0 +1,22 @@
+class Ruby_sequel < PACKMAN::Package
+  url PACKMAN.gem_source+'sequel-4.24.0.gem'
+  sha1 '6710a3ba824eabfbdfeeb6702531231e3070af02'
+  version '4.24.0'
+
+  label :try_system_package_first
+  label :not_set_bashrc
+
+  depends_on 'sqlite'
+
+  def install
+    PACKMAN.gem "install sequel-#{version}.gem"
+  end
+
+  def remove
+    PACKMAN.gem 'uninstall -x sequel'
+  end
+
+  def installed?
+    PACKMAN.is_gem_installed? 'sequel', version
+  end
+end
