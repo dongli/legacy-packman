dbd0c28d413c08113d2ab33d76ed2cc1d80830e2
Zhang Kai Yu <yeannylam@gmail.com>
2015-07-21 12:22:32 +0800

    Fix kroman recipe

--- packages/kroman.rb
+++ packages/kroman.rb
@@ -1,8 +1,10 @@
 class Kroman < PACKMAN::Package
-  url "https://github.com/cheunghy/kroman/archive/\
-fafae2ac45ddba85871d24eeeb117b9613a93f3f.tar.gz"
-  sha1 "a1f1cac4d80d676e3b032f3d8c0e11708bdb08d2"
+  url 'https://github.com/cheunghy/kroman/archive/v1.0.tar.gz'
+  sha1 'f3a9341c4c5b68ed8183a94d00875e41c81c395b'
   version '1.0'
+  filename 'kroman-1.0.tar.gz'
+
+  label :compiler_insensitive
 
   def install
     PACKMAN.run "make install PREFIX=#{prefix}"
