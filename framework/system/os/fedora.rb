c72b9f71a1ef5877da13a11535285f78ccce16f9
Li Dong <dongli@lasg.iap.ac.cn>
2015-09-06 11:04:42 +0800

    Fix Fedora os version matching

--- framework/system/os/fedora.rb
+++ framework/system/os/fedora.rb
@@ -2,7 +2,7 @@ module PACKMAN
   class Fedora < RedHat
     type :Fedora
     check :version do
-      `cat /etc/*-release`.match(/VERSION_ID=(\d+)/)[1]
+      `cat /etc/*-release`.match(/Fedora release (\d+)/)[1]
     end
   end
 end
