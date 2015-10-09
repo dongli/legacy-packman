9bc2a2d6e84d81f88c481a509e324042f6e9283a
Li Dong <dongli@lasg.iap.ac.cn>
2015-10-10 07:48:38 +0800

    Fix curl

--- packages/curl.rb
+++ packages/curl.rb
@@ -1,20 +1,14 @@
 class Curl < PACKMAN::Package
   url 'http://curl.haxx.se/download/curl-7.44.0.tar.bz2'
-  sha1 '2123b6f0ce7729d07f72a6746c487bdfe35c3cc1'
+  sha1 '879a186944e7b06e619a2eb07cef729b5702345c'
   version '7.44.0'
-
-  binary do
-    compiled_on :Mac, '=~ 10.11'
-    compiled_by :c => [ :llvm, '=~ 7.0' ]
-    sha1 '4b14dad17489424b259356fe68066d4623594600'
-    version '7.44.0'
-  end
 
   depends_on :zlib
   depends_on :libressl
 
   def install
     PACKMAN.prepend_env 'PKG_CONFIG_PATH', "#{Libressl.lib}/pkgconfig"
+    PACKMAN.handle_unlinked Libressl
     args = %W[
       --prefix=#{prefix}
       --with-ssl=#{Libressl.prefix}
